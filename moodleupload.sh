09e82ebfa554418baa9ea28feea4e70498eff9cb
Benito van der Zander <benito@tcs.uni-luebeck.de>
2015-07-28 18:17:13 +0200

    remove (=hide) old uploads

--- moodleupload.sh
+++ moodleupload.sh
@@ -17,7 +17,7 @@ if [[ -z "$1" ]]; then texfile=$( (ls *.sheet; ls *.tex) | sort | tail -1)
 else texfile="$1"
 fi
 
-~/xidel  "$texfile" --variable 'course,user,pass' -e '
+~/xidel "$texfile" --variable 'course,user,pass' -e '
   $sheet := $raw, 
   $option := function($name, $default) {
     let $res := extract($sheet, "% *Moodle +"||$name||" *= *(.*)", 1) return
@@ -37,7 +37,12 @@ fi
   $assignmenttitle := $title  || if ($allow-file-upload) then "" else if ($slang eq "english") then " (results)" else " (Ergebnisse)", 
   $spoints := sum(tokenize(if (contains($sheet, "begin{homework}")) then substring-after($sheet, "begin{homework}") else $sheet, $line-ending) !  extract(., "^[^%]*credits=([^\],%]*)", 1) ! tokenize(., "[a-zA-Z ]+") [.] ! number())  '  \
    'https://moodle.uni-luebeck.de/' -f 'form(//form, {"username": $user, "password": $pass})' \
-  [ 'https://moodle.uni-luebeck.de/course/view.php?id={$course}' -e 'section := (((//span[contains(@class, "accesshide") and contains(.,  "Aufgabe")])[last()]/following::li[contains(@class, "section")])[1]/extract(@id, "[0-9]+"), $snumber)[1]' ]  \
+  [ 'https://moodle.uni-luebeck.de/course/view.php?id={$course}' --allow-repetitions \
+    -e 'section := (((//span[contains(@class, "accesshide") and contains(.,  "Aufgabe")])[last()]/following::li[contains(@class, "section")])[1]/extract(@id, "[0-9]+"), $snumber)[1]' \
+    -f 'form((//form)[1], "edit=on")' \
+    -f 'css("div.activityinstance")[contains(a/@href, "view.php") and .//text()/normalize-space() = $title]/..//a[matches(@href, "mod[.]php.*hide")]'\
+    -e '()' \
+  ]  \
   [ 'https://moodle.uni-luebeck.de/course/modedit.php?add=resource&type=&course={$course}&section={$section}&return=0&sr=0' -e 'infoForm := form(//form[contains(@action, "modedit")], {"name": $title, "introeditor[text]": $description})' -f '//noscript//object/@data[contains(., "env=filemanager")]' -f '//a/@href[contains(., "filepicker")]'  -f '//a[contains(., "hochladen")]'  -f 'form(//form, {"repo_upload_file": {"file": $uploadFilename}})' -f '//a/@href[not(starts-with(., "#"))]' -f '$infoForm' -e 'css("p.activity")[last()]' ] \
   [ 'https://moodle.uni-luebeck.de/course/modedit.php?add=assign&type=&course={$course}&section={$section}&return=0&sr=0' -f 'form((//form)[1], {"name": $assignmenttitle, "introeditor[text]": $assignmenttitle, "duedate[day]": $sdeadline[1], "duedate[month]": $sdeadline[2], "duedate[year]": $sdeadline[3], "duedate[hour]":  $hour, "grade[modgrade_point]": $spoints, "assignsubmission_file_enabled": if ($allow-file-upload) then "1" else ""})[$make-assignment]' -e 'css("span.instancename") [contains(., string($snumber))]' -e 'span.error' ]
 
