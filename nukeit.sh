01e39184dd48d77487d19b5f68157ffbaebf747a
benibela <benito@benibela.de>
2016-08-02 15:48:16 +0200

    use bash and do not kill Gruppenwal

--- nukeit.sh
+++ nukeit.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 if [[ -z "$course" ]]; then echo need course; exit; fi
 if [[ -z "$user" ]]; then export user=$(cat ~/.moodleuser); fi
 if [[ -z "$user" ]]; then echo Need moodle \$user; exit; fi
@@ -15,7 +15,7 @@ read
    'https://moodle.uni-luebeck.de/' -f 'form(//form, {"username": $user, "password": $pass})' \
    'https://moodle.uni-luebeck.de/course/view.php?id={$course}' \
    -f '//form[.//input[@name="edit" and @value="on"]]' \
-   -f '(//a[@data-action="delete"][not(ancestor::div[2]/css("span.accesshide")/normalize-space() = ("Forum", "Textseite"))])' \
+   -f '(//a[@data-action="delete"][not(ancestor::div[2]/css("span.accesshide")/normalize-space() = ("Forum", "Textseite", "Gruppenwahl"))])' \
    -f '//form[contains(@action, "mod.php")]'  
 #   -e / --xml
 
