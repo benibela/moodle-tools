91ea14ce7fa9b0b0b74e14e17f84b267c27364db
benibela <benito@benibela.de>
2016-10-05 09:43:28 +0200

    auto load pass

--- moodleupload.sh
+++ moodleupload.sh
@@ -2,6 +2,7 @@
 #if [[ -z "$course" ]]; then echo need course; exit; fi
 if [[ -z "$user" ]]; then export user=$(cat ~/.moodleuser); fi
 if [[ -z "$user" ]]; then echo Need moodle \$user; exit; fi
+if [[ -z "$pass" ]]; then export pass=$(cat ~/.moodlepass); fi
 if [[ -z "$pass" ]]; then echo "Enter password for $user"; read -r pass; fi
 #export course
 export user
