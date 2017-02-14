b181375551991823ca7f98bfead7e0162ced9ec7
Benito van der Zander <benito@tcs.uni-luebeck.de>
2017-02-14 15:21:26 +0100

    mention time and review id, when notifying about submission

--- autograding/autoseminarupload.sh
+++ autograding/autoseminarupload.sh
@@ -43,8 +43,12 @@ export DIR
               return simple-str-sim($st[1], $tt[1]) + simple-str-sim($st[count($st)], $tt[count($tt)])
             }; 
             let $sections := unparsed-text-lines("tmpsections" || $course)
-            for $submission in unparsed-text-lines("submissions/active"||$exercise)
-            let $name := $submission!normalize-space(substring-before(.,"§"))
+            let $history := if (file:exists("submissions/history"||$exercise)) then file:read-text-lines("submissions/history"||$exercise)![tokenize(., "§")!normalize-space()] else ()
+            for $submission in file:read-text-lines("submissions/active"||$exercise)
+            let $cols := tokenize($submission, "§")
+            where count($cols) ge 2
+            let $name := normalize-space($cols[1])
+            let $version := count($history[ .(1) = $name ])
             let $filename := "submissions/files/" || extract($submission,"([0-9]+/[^/?]+)([?].*)? *$", 1)
             let $student := exactly-one($utils:students-normal[.(1) = $name])
             let $assignment := $student(3)
@@ -59,7 +63,7 @@ export DIR
              system(x"{$DIR}/moodleupload.sh ""{$uploadInfoFile}"""),
              let $reviewers := utils:get-reviewers-moodle-id($student)
              for $reviewer at $id in $reviewers return
-             system(x"{$DIR}/message.sh {$reviewer} ""Das zum Reviewen vorgesehene Thema {$assignmenttitle} wurde abgegeben {if (count($reviewers) = 2) then x" (Review {$id})" else ()}.""")
+             system(x"{$DIR}/message.sh {$reviewer} ""Das zum Reviewen {if (count($reviewers) ge 2) then x" (<b>für Review {$id}</b>)" else ()}  vorgesehene Thema {$assignmenttitle} { if ($version ge 2) then x" ({format-integer($version, "w;o", "de")} Fassung)" else ()} wurde am { $cols[2] } abgegeben.""")
                 )' 
   
 #  || " => " ||
