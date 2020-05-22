d533087c281a6d35c210ed6f1f1f5f4d537576c8
benibela <benito@benibela.de>
2020-05-22 14:34:22 +0200

    add scripts to work with downloaded group submissions

---
+++ groupgrading/mergegradedandduplicates.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#Create a "merged" subdirectory containing:
+#  all files of the current directory
+#  all files removed by moveduplicates.sh, but replaced by the file that has replaced the duplicate in the current dir (assuming all duplicates are named GROUP...-). 
+xidel --xquery 'file:create-dir("merged"), 
+                let $here := file:list("./")[file:is-file(.)] 
+                let $duplicates := file:list("./duplicates") 
+                for $h in $here 
+                let $group := substring-before($h, "-") 
+                return (file:copy($h, "merged/"), 
+                        for $d in $duplicates 
+                        let $group2 := substring-before($d, "-") 
+                        where $group = $group2 
+                        return file:copy($h, "merged/" || $d))'
+
---
+++ groupgrading/moveduplicates.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#Move duplicated files in the current directory to a "duplicates" subdirectory
+xidel --xquery 'file:create-dir("duplicates"), 
+                for $f in file:list(.) group by $group := substring-before($f, "-") || file:size($f) for $t in tail($f) 
+                return file:move($t, "duplicates/"|| $t)
