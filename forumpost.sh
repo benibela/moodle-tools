e056df666d2e1603fa3c7bb026f2b7f3d0547c8b
benibela <benito@benibela.de>
2020-05-22 14:19:52 +0200

    new options for automated form posts

--- forumpost.sh
+++ forumpost.sh
@@ -4,21 +4,19 @@ source "$DIR/common.sh"
 #Call it with message.sh forum id < text to post text. first line is title
 
 export forum=$1
+export subject=$2
 export message=$(cat)
+export messageformat
+export options=$3
+ 
 
-
-
-moodle --variable forum,message \
+moodle --variable forum,subject,message,messageformat,options \
    'https://moodle.uni-luebeck.de/mod/forum/post.php?forum='$forum                          \
    -f '
      let $form := //form[contains(@action, "post.php")],
-         $message := x:lines($message),
-         $subject := head($message),
-         $message := tail($message),
-         $has-options := matches($message[1], "\{.*\}"),
-         $options := if ($has-options) then parse-json(head($message)) else (),
-         $message := if ($has-options) then tail($message) else $message
-     return form($form, map:merge(({"subject": $subject, "message[text]": join( $message, $line-ending) }, $options)))
+         $options := moodle:prepend-uri-options($options, {"subject": $subject}),
+         $request := moodle:modedit-form($form, (), $message, $messageformat, $options)
+     return $request     
    ' -e 'css(".alert")'
    #--download - -e //title
    
