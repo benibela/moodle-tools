f392a83638af5990c9585e6038440644f5dad621
benibela <benito@benibela.de>
2020-06-16 19:24:57 +0200

    fix group grading syntax error

--- groupgrading/moveduplicates.sh
+++ groupgrading/moveduplicates.sh
@@ -2,4 +2,4 @@
 #Move duplicated files in the current directory to a "duplicates" subdirectory
 xidel --xquery 'file:create-dir("duplicates"), 
                 for $f in file:list(.) group by $group := substring-before($f, "-") || file:size($f) for $t in tail($f) 
-                return file:move($t, "duplicates/"|| $t)
+                return file:move($t, "duplicates/"|| $t)'
