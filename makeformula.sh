a2e69fea2c39e9d7911ae789b77a2182cb44283b
benibela <benito@benibela.de>
2020-06-30 01:00:00 +0200

    change grading formula creator to check for weekly course view. Exclude alan contest exercises from formula

--- makeformula.sh
+++ makeformula.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
+# Create a grading formula for a course. Condition: pass if at most two exercise assignments were failed.
+# Input as environment variable
+#   course
+
 DIR="$( cd "$( dirname -- "$(readlink -f -- "${BASH_SOURCE[0]}")" )" && pwd )"
 source "$DIR/common.sh"
 
 #https://moodle.uni-luebeck.de/mod/assign/view.php?id=112210
 #https://moodle.uni-luebeck.de/course/modedit.php?update=112210&return=1
 
-moodlewithcourse 'https://moodle.uni-luebeck.de/course/view.php?id={$course}' -f '//a[contains(@href, "mod/assign/view")]/replace(@href, "mod/assign/view.php[?]id=", "course/modedit.php?update=")'  \
+moodlewithcourse 'https://moodle.uni-luebeck.de/course/view.php?id={$course}' \
+  -f 'moodle:course-edit-follow(/)' \
+  -f '//a[contains(@href, "mod/assign/view")]/replace(@href, "mod/assign/view.php[?]id=", "course/modedit.php?update=")'  \
    --xquery 'let $p := x:request-decode(form(//form[contains(@action, "modedit")])).params
              let $id := ($p("cmidnumber")[.], "MISSING ID")[1]
-             where $p("visible") ne "0" and not(contains($id, "Klausur")) and not (contains($p("name"), "Klausurtermin"))
+             where $p("visible") ne "0" and not(contains($id, "Klausur")) and not (matches($p("name"), "Klausurtermin|Einreichung Ihres Codes"))
              let $maxpoints := $p("grade[modgrade_point]")
              let $okpoints := $p("gradepass")
              return
@@ -17,6 +23,9 @@ moodlewithcourse 'https://moodle.uni-luebeck.de/course/view.php?id={$course}' -f
   let $lines := x:lines($raw)[contains(., "min")]   
   let $allowedtofail := 2
   return ( x"=min( {join($lines, "+")} - {count($lines) - $allowedtofail - 2} ; 2) ",
-  "","","Anzahl an relevanten Blättern: "||count($lines))
+  x"
+  
+  Anzahl an relevanten Blättern: {count($lines)}
+  Einfluss der Anzahl in der Formel: {count($lines)} - {$allowedtofail} - 2 ; 2))")
 '
 
